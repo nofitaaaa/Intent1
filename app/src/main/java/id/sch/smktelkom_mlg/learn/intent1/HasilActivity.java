c6f23f47e23dad5275dcf4a44ba4aed7f4fa9430
nofita <nofitamahfudiyah@gmail.com>
2016-10-06 07:09:08 +0700

    Initial Commit startActivity Part 1 onBackPressed Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -2,12 +2,24 @@ package id.sch.smktelkom_mlg.learn.intent1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HasilActivity extends AppCompatActivity {
+public class HasilActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+
+        findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
